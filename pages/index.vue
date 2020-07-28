4764e6dc03f987b62883bd253509fc23069df2e9
Roman Kuepper <rrkuepper@gmail.com>
2020-07-28 14:50:42 +0200

    setup project correctly

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,25 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        demo
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <b-jumbotron class="text-center">
+      <h3>Welcome to Nuxt.js auth example</h3>
+      <div class="mt-1">
+        <template v-if="$auth.$state.loggedIn">
+          <b-btn class="ml-3" variant="info" to="/secure">
+            Secure
+          </b-btn>
+          <b-btn class="ml-3" variant="danger" @click="$auth.logout()">
+            Logout
+          </b-btn>
+        </template>
+        <b-btn v-else variant="success" to="/login">
+          Login
+        </b-btn>
       </div>
+    </b-jumbotron>
+
+    <div>
+      User status:
+      <b-badge>{{ $auth.$state.loggedIn ? 'Logged In' : 'Guest' }}</b-badge>
     </div>
   </div>
 </template>
-
-<script>
-export default {}
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
